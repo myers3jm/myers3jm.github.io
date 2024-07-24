85086cc6ff7bfaf44c1429731d463d4de691a476
J. Matthew Myers <myers3jm@mail.uc.edu>
2024-07-24 19:21:42 -0400

    Adding React framework
    This change adds the React framework, and places a component on the homepage

---
+++ src/Component.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import '../custom.css'
+
+function Component() {
+  return (
+    <>
+      <p class="react">This text was created with React!</p>
+    </>
+  )
+}
+
+export default Component
