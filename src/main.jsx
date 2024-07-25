5a5e9f69a7f53e0539c207dae62fa2f0ab120065
J. Matthew Myers <myers3jm@mail.uc.edu>
2024-07-24 23:53:19 -0400

    Convert email display to React component
    This change moves the show/hide email functionality to a react component

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Component from './Component.jsx'
+import ShowEmail from './Email.jsx'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('react')).render(
   <React.StrictMode>
     <Component />
+  </React.StrictMode>,
+)
+
+ReactDOM.createRoot(document.getElementById('email-section')).render(
+  <React.StrictMode>
+    <ShowEmail />
   </React.StrictMode>,
 )
